a06f7fc4cfb1263827eef697918ab24815ec6856
AyushKushwaha2510 <ayush.kushwaha23138@gmail.com>
2026-07-12 19:50:03 +0530

    Add CORS middleware to enable cross-origin requests

--- app/main.py
+++ app/main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.router import api_router
 from app.core.config import settings
 
 app = FastAPI(
     title=settings.APP_NAME,
     version="1.0.0",
+)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://www-healthsync.vercel.app",
+        "https://healthsync-production-ee66.up.railway.app"
+        "http://localhost:4000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(api_router)
