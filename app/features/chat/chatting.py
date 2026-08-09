a8a3c552c389cd2a672fa09069a1a85cabacfb40
AyushKushwaha2510 <ayush.kushwaha23138@gmail.com>
2026-08-09 22:49:37 +0530

    Refactor send_message method to accept a list of Message objects

--- app/features/chat/chatting.py
+++ app/features/chat/chatting.py
@@ -1,19 +1,20 @@
+from typing import List
 from app.ai.llm.gemini import gemini_provider
 from app.ai.llm.provider import LLMProvider
 from app.features.chat.prompts import SEND_MESSAGE_PROMPT
-from app.features.chat.schema import Chat
+from app.features.chat.schema import Chat, Message, SendMessage
 
 
 class Chatting:
     def __init__(self, llm: LLMProvider) -> None:
         self.llm = llm
 
-    async def send_message(self, message: str) -> str:
+    async def send_message(self, messages: List[Message]) -> str:
         """
         Send Message to LLM
         """
 
-        prompt = SEND_MESSAGE_PROMPT.invoke({"message": message})
+        prompt = SEND_MESSAGE_PROMPT.invoke({"message": messages})
 
         res = await self.llm.generate_structured(prompt.to_string(), Chat)
 
